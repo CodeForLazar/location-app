4c16cb519c9d4dca65cf9e6f1cf51dd4bf40b99c
Lazar Kiridjievski <l.kiridzievski@gmail.com>
2023-03-06 00:36:34 +0100

    /

--- backend/src/validators/placesValidators.ts
+++ backend/src/validators/placesValidators.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express"
-import { check, validationResult } from "express-validator";
+import { body, validationResult } from "express-validator";
 import HttpError from "../utils/HttpError";
 
 const title = "title";
@@ -7,13 +7,16 @@ const description = "description";
 const address = "address";
 
 export const createPlaceValidator = [
-   check(title).notEmpty(),
-   check(description).isLength({ min: 5 }),
-   check(address).notEmpty(),
+   body(title, "title is required")
+      .notEmpty(),
+   body(description)
+      .isLength({ min: 5 }),
+   body(address, "adress is required")
+      .notEmpty(),
    (req: Request, res: Response, next: NextFunction) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-         const message = JSON.stringify(errors.mapped())
+         const message = errors.array().map(err => err.msg);
          throw next(new HttpError(`${message}`, 422));
       }
       next();
